2b0e1dedd95b155336eb50a3051005aca44c6532
Chason Choate <chason@bondware.com>
2013-08-19 13:43:21 -0500

    Added devise gem. Makes tweaks for devise.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+gem 'devise'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 SimpleForum::Application.configure do
+    config.action_mailer.default_url_options = { :host => 'localhost:3002' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
