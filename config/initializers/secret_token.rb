3735ee3c8f2a158411a31f723b7e90fed4fef66a
Chason Choate <chason@bondware.com>
2013-08-19 13:19:32 -0500

    Adding rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleForum::Application.config.secret_key_base = '9f974ed9241f3824242c3fb6f75b5af389812c77756d7359ce45651a5afde7ae93a7671093a5c619aa62591c92e1bc0265b4b2e97716ec966eb34e9faf707449'
